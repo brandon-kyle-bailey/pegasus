5d08ac03676c020397365c8b4084199490ada420
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2024-04-13 02:44:34 -0300

    give custom event emitter an instance id

--- src/cqrs/event-emitter.cqrs.ts
+++ src/cqrs/event-emitter.cqrs.ts
@@ -1,15 +1,18 @@
 import EventEmitter from 'events';
 import { Injectable } from '../di/injectable.di';
+import { v4 } from 'uuid';
 
 
 @Injectable()
 export class CustomEventEmitter {
   private static instance: EventEmitter;
+  static instanceId: string;
   private constructor() {}
 
   static getInstance(): EventEmitter {
     if (!CustomEventEmitter.instance) {
       CustomEventEmitter.instance = new EventEmitter();
+      CustomEventEmitter.instanceId = v4();
     }
     return CustomEventEmitter.instance;
   }
